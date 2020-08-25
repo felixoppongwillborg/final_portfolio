4d7d2ef42347a3131e05577bd068b72484e57f1a
felixoppongwillborg <fwlixw@gmail.com>
2020-08-25 20:53:18 +0200

    change ids in cypress test, added buttons on interface. all test go green.

--- cypress/integration/userCanSeeWebpageInterface.feature.js
+++ cypress/integration/userCanSeeWebpageInterface.feature.js
@@ -2,9 +2,9 @@ describe("User can see web page  interface", () => {
   it ('user can see landingpage', () => {
   cy.visit("localhost:3000");
   cy.get("#title").should('contain', 'Felix Oppong Willborg');
-  cy.get("#aboutButton").should('contain', 'About me');
-  cy.get("#projectsButton").should('contain', 'Projects');
-  cy.get("#cvButton").should('contain', 'Resumé');
-  cy.get("mySundayButton").should('contain', 'Act out my Sunday')
+  cy.get("#aboutMeBtn").should('contain', 'About me');
+  cy.get("#projectsBtn").should('contain', 'Projects');
+  cy.get("#cvBtn").should('contain', 'Resumé');
+  cy.get("#mySundayBtn").should('contain', 'Act out my Sunday')
   })
 })
--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,10 @@ const Home = () => {
   return (
     <div>
       <h1 id="title">Felix Oppong Willborg</h1>
+      <button id="aboutMeBtn">About me</button>
+      <button id="projectsBtn">Projects</button>
+      <button id="cvBtn">Resumé</button>
+      <button id="mySundayBtn">Act out my Sunday</button>
     </div>
   )
 }
