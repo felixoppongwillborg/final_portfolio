2722bca8da411971daba842d4d1825550009861f
felixoppongwillborg <fwlixw@gmail.com>
2020-09-16 22:00:35 +0200

    added container and segment to about me page

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { Link } from "react-scroll";
-import { Button } from "semantic-ui-react";
+import { Button, Segment, Container } from "semantic-ui-react";
 
 const AboutMe = () => {
   return (
     <div id="AboutMe-section">
-      <h1 id="subTitle">About me!</h1>
-      <h3 id="text">
-        {" "}
-        Junior Web Developer, with previous experience within the insurance,
-        retail and art industry. During the past 10 years, I have acquired a
-        broad set of both hard and soft skills applicable across different
-        industries. Having extensive experience working in B2B and B2C roles.
-        Completed an intensive coding bootcamp at Craft Academy during summer
-        2020. I have now gained knowledge working with both test and
-        behavior-driven development applying the Agile methodology. Hands-on,
-        innovative and a problem solver with keen attention to details.
-      </h3>
+      <Container>
+        <Segment.Group raised>
+          <h1 id="subTitle">About me!</h1>
+          <h3 id="text">
+            {" "}
+            Junior Web Developer, with previous experience within the insurance,
+            retail and art industry.
+            <br />
+            During the past 10 years, I have acquired a broad set of both hard
+            and soft skills applicable across different industries. Having
+            extensive experience working in B2B and B2C roles.
+            <br />
+            Completed an intensive coding bootcamp at Craft Academy during
+            summer 2020. I have now gained knowledge working with both test and
+            behavior-driven development applying the Agile methodology.
+            Hands-on, innovative and a problem solver with keen attention to
+            details.
+          </h3>
+        </Segment.Group>
+      </Container>
       <Link
         to="Projects-section"
         activeClass="active"
